14d2ec4c4f7535b53518eba74edacce419dd6fdf
Dayong Wu <dayong@bu.edu>
2022-05-20 14:58:23 -0400

    added variables definition code

---
+++ go_basics.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+// CANNOT use colon to define variables outside a function
+// these are not "global" variables. They are just variables within a package
+var (
+	aa = 3
+	ss = "kkk"
+)
+
+func variableZeroValue() {
+	var a int
+	var s string
+	//fmt.Println(a, s)
+	fmt.Printf("%d, %q \n", a, s)
+}
+
+func variableInitialValue() {
+	var a, b int = 3, 4
+	var s string = "KKY"
+	fmt.Println(a, b, s)
+}
+
+func variableWithNoTypes() {
+	var a, b, s = 3, 4, "KKY" // can write variables with different types in the same line
+	//var s = "KKY"
+	fmt.Println(a, b, s)
+}
+
+func variableSimplerWay() {
+	a, b, s := 3, 4, "KKY" // use colon instead of "var". BETTER WAY! DON'T USE var!
+	b = 5
+	fmt.Println(a, b, s)
+}
+
+func main() {
+	//fmt.Println("hello")
+	//fmt.Println(runtime.GOARCH) // check your code is running on what architecture
+	variableZeroValue()
+	variableInitialValue()
+	variableWithNoTypes()
+	variableSimplerWay()
+	fmt.Println(aa, ss)
+}
